f3c23c35c843d9fce04401a1281363cad689a637
Fuga14 <vladyslavpereder@gmail.com>
2025-03-21 00:29:36 +0200

    ci: cfg settings

---
+++ hardhat.config.ts
@@ -0,0 +1,50 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : "";
+
+// API keys
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const BASESCAN_API_KEY = process.env.BASESCAN_API_KEY || "";
+
+// RPC URLs
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const BASE_RPC_URL = process.env.BASE_RPC_URL || "";
+
+const config: HardhatUserConfig = {
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.22",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200
+                    }
+                }
+            }
+        ]
+    },
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: PRIVATE_KEY !== "" ? [PRIVATE_KEY] : []
+        },
+        baseSepolia: {
+            url: BASE_RPC_URL,
+            accounts: PRIVATE_KEY !== "" ? [PRIVATE_KEY] : []
+        }
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+            baseSepolia: BASESCAN_API_KEY
+        }
+    }
+};
+
+export default config;
